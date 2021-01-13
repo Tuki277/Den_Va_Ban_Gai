fb6c59c45e7b78629fb7bbea1c1cc33f918ed074
Tran Hoang Huy <huybeos2707@gmail.com>
2021-01-14 03:36:04 +0700

    them 1 so chuc nang

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,19 +8,19 @@ cloudinary.config({
     api_secret: 'FslI1DN5Eom0AWZe-9VeklvAwgQ'
 })
 
-const getAdd = async(req, res, next) => {
+exports.getAdd = async(req, res, next) => {
     res.render('add')
 }
 
-const getAddImage = async(req, res, next) => {
+exports.getAddImage = async(req, res, next) => {
     res.render('addImage')
 }
 
-const getSite = async (req, res, next) => {
+exports.getSite = async (req, res, next) => {
     res.render('site')
 }
 
-const addPost = async(req, res, next) => {
+exports.addPost = async(req, res, next) => {
     const {
         text
     } = req.body
@@ -40,7 +40,7 @@ const addPost = async(req, res, next) => {
     res.redirect('/add')
 }
 
-const addImage = async(req, res, next) => {
+exports.addImage = async(req, res, next) => {
     const image = req.file.path.split('\\').slice(1).join('\\')
 
     const result = await cloudinary.uploader.upload(req.file.path)
@@ -55,15 +55,87 @@ const addImage = async(req, res, next) => {
     res.redirect('/addImage')
 }
 
-const getManage = async(req, res, next) => {
-    res.render('manage')
+exports.getManage = async(req, res, next) => {
+
+    const textPost = await textModels.find()
+
+    const imagePost = await imageModels.find()
+
+    res.render('manage', { data1 : textPost, data2 : imagePost })
 }
 
-module.exports = {
-    getAdd,
-    addPost,
-    addImage,
-    getAddImage,
-    getSite,
-    getManage
+exports.deteleImage = async(req, res, next) => {
+    const { id } = req.params
+    console.log(id)
+    const deteteImg = await imageModels.findByIdAndDelete(id)
+    res.redirect('/manage')
+}
+
+exports.deteleText = async(req, res, next) => {
+    const { id } = req.params
+    console.log(id)
+    const deteteText = await textModels.findByIdAndDelete(id)
+    res.redirect('/manage')
+}
+
+exports.getEditImgae = async (req, res, next) => {
+    const { id } = req.params
+
+    const dataImage = await imageModels.findById(id)
+
+    console.log(dataImage._id)
+
+    res.render('editImage', { data : dataImage })
+}
+
+exports.getEditText = async (req, res, next) => {
+    const { id } = req.params
+
+    const textPost = await textModels.findById(id)
+
+    console.log(textPost)
+
+    res.render('editText', { textPost : textPost })
+}
+
+exports.editText = async (req, res, next) => {
+    const { id } = req.params
+
+    console.log(req.body)
+
+    const {
+        text
+    } = req.body
+    console.log(text)
+
+    // const image = req.file.path.split('\\').slice(1).join('\\')
+
+    // const result = await cloudinary.uploader.upload(req.file.path)
+
+    const updatePost = {
+        text : text
+        // image : result.url
+    }
+
+    // const update = await textModels.findByIdAndUpdate(id, updatePost)
+
+    // res.redirect('/manage')
+}
+
+exports.editImage = async (req, res, next) => {
+    const { id } = req.params
+
+    const image = req.file.path.split('\\').slice(1).join('\\')
+
+    const result = await cloudinary.uploader.upload(req.file.path)
+
+    const updatePost = {
+        image : result.url
+    }
+
+    console.log(updatePost)
+
+    const update = await imageModels.findByIdAndUpdate(id, updatePost)
+
+    res.redirect('/manage')
 }
